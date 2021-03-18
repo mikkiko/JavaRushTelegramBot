03ee7c0a27ca3a17615e36df2d1e8bbaeb33ed67
MykolaKostyshyn <icarus787.@gmail.com>
2021-03-18 23:31:01 +0100

    JRTB-1: added repository layer.

--- src/main/java/com/github/javarushcommunity/jrtb/command/StopCommand.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/StopCommand.java
@@ -1,24 +1,33 @@
 package com.github.javarushcommunity.jrtb.command;
 
 import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 /**
  * Stop {@link Command}
  */
 
-public class StopCommand implements Command{
+public class StopCommand implements Command {
 
     private final SendBotMessageService service;
+    private final TelegramUserService telegramUserService;
 
     public final static String STOP_MESSAGE = "Деактивировал все ваши подписки \uD83D\uDE1F.";
 
-    public StopCommand(SendBotMessageService service) {
+    public StopCommand(SendBotMessageService service, TelegramUserService telegramUserService) {
         this.service = service;
+        this.telegramUserService = telegramUserService;
     }
 
     @Override
     public void execute(Update update) {
-        service.sendMessage(update.getMessage().getChatId().toString(), STOP_MESSAGE);
+        String chatId = update.getMessage().getChatId().toString();
+        service.sendMessage(chatId, STOP_MESSAGE);
+        telegramUserService.findByChatId(chatId)
+                .ifPresent(it -> {
+                    it.setActive(false);
+                    telegramUserService.save(it);
+                });
     }
 }
